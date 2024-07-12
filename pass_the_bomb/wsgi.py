51a0db294c8b2c1f11f97c540f238675ff5f3050
Oleg Pak <pak.o99@mail.ru>
2024-07-12 20:03:36 +0300

    added django and startproject

---
+++ pass_the_bomb/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for pass_the_bomb project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pass_the_bomb.settings')
+
+application = get_wsgi_application()
